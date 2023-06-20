e7f5d5dbbe4a54c4dc90063d668cd5140920983a
luigimvargas <luigimvargas@gmail.com>
2023-06-20 01:48:19 -0400

    Ability to incorporate multiple datasets

---
+++ deepLabCut_FiltLaser.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+"""
+This function is for analyzing csv outputs from deeplabcut. The data are filtered
+for possible errors and then an analysis of movement is performed.
+
+When running this code, two pop up windows will appear to prompt a file input.
+The first input should be the coordinate data as a csv output from deeplabcut.
+The second input is a matlab matrix. Each row of this matrix represents a 
+distinct laser event. The first column of each row is the time that the laser
+turns on & the second column is the time that the laser turns off. 
+
+    In this case, the times are encoded in seconds whereas the deeplabcut 
+    coordinates are encoded in frames per second. Seconds are multiplied by 
+    frames per second (fps) to account for this difference. Be sure to account 
+    for this step for any other situations.
+
+Moreover, we are using a 0.5 second window around the laser turning on and off
+for our analyses here. I did this because my laser lengths are 1.5 seconds.
+If your laser time is much shorter, this window might be too long. Change 
+halfSecFrames as necessary to shorten or elongate the window.
+ 
+Code by Luigim Vargas
+May 17th, 2023
+"""
+import pandas as pd
+import numpy as np
+import scipy.io as sio
+
+def deepLabCut_FiltLaser(coordinate_path,laser_path,fps,num_events):
+    #Import the data and change to numpy array
+    #Infer animal name from coordinate_path
+    ofData=pd.read_csv(coordinate_path,header=[1,2])
+    ofData=np.asarray(ofData)
+    animalName=coordinate_path.split('/')[-1].split('_')[0]
+    #%% Cell for cleaning coordinate data
+    #Cut data length to 20 minutes or, at 10fps, 12000 frames
+    # (Make it start at 10 seconds prior to first laser)
+    noseCords=ofData[:,[1,2]]
+    bodyCords=ofData[:,[4,5]]
+    tailCords=ofData[:,[7,8]]
+    
+    #Basic distance formula we will be using repeatedly
+    def distance(x1,x0,y1,y0):
+        result=np.sqrt((x1-x0)**2 +(y1-y0)**2 )
+        return result
+    
+    #First calculate the change in distance for each point's X & Y coordinate
+    noseDeltas=np.zeros(np.shape(noseCords[:,0]))
+    bodyDeltas=np.zeros(np.shape(bodyCords[:,0]))
+    tailDeltas=np.zeros(np.shape(tailCords[:,0]))
+    for i in range(noseCords.shape[0]-1):
+        noseDeltas[i]=distance(noseCords[i+1,0],noseCords[i,0],noseCords[i+1,1],noseCords[i,1])
+            
+        bodyDeltas[i]=distance(bodyCords[i+1,0],bodyCords[i,0],bodyCords[i+1,1],bodyCords[i,1])
+            
+        tailDeltas[i]=distance(tailCords[i+1,0],tailCords[i,0],tailCords[i+1,1],tailCords[i,1])
+    
+    #Calculate the standard deviation & means of change for each coordinate
+    noseDeltaMeans=np.mean(noseDeltas)
+    noseDeltaSDevs=np.std(noseDeltas)
+    
+    bodyDeltaMeans=np.mean(bodyDeltas)
+    bodyDeltaSDevs=np.std(bodyDeltas)
+    
+    tailDeltaMeans=np.mean(tailDeltas)
+    tailDeltaSDevs=np.std(tailDeltas)
+    
+    #For points that change > std, set X & Y of ofData to previous point
+    noseEditFlag=np.zeros(noseCords.shape)
+    bodyEditFlag=np.zeros(bodyCords.shape)
+    tailEditFlag=np.zeros(tailCords.shape)
+    
+    noseDataFiltered=noseCords
+    bodyDataFiltered=bodyCords
+    tailDataFiltered=tailCords
+    
+    for i in range(noseCords.shape[0]-1):
+        if noseDeltas[i]> (noseDeltaMeans + noseDeltaSDevs*2.58):
+            noseDataFiltered[i,0]=noseCords[i-1,0]
+            noseDataFiltered[i,1]=noseCords[i-1,1]
+            noseEditFlag[i]=1
+                
+        if bodyDeltas[i]> (bodyDeltaMeans + bodyDeltaSDevs*2.58):
+            bodyDataFiltered[i,0]=bodyCords[i-1,0]
+            bodyDataFiltered[i,1]=bodyCords[i-1,1]
+            bodyEditFlag[i]=1
+        
+        if tailDeltas[i]> (tailDeltaMeans + tailDeltaSDevs*2.58):
+            tailDataFiltered[i,0]=tailCords[i-1,0]
+            tailDataFiltered[i,1]=tailCords[i-1,1]
+            tailEditFlag[i]=1
+            
+    #%% Cell for calculating Distance, Speed, & Turning        
+            
+    #With data filtered, calculate distance using tailBase
+    tailTotalDist=0
+    for i in range(tailCords.shape[0]-1):
+        tailTotalDist=tailTotalDist+distance(tailDataFiltered[i+1,0],tailDataFiltered[i,0],tailDataFiltered[i+1,1],tailDataFiltered[i,1])
+    
+    mouseVelocity=np.zeros(tailCords.shape[0])
+    #Calculate Speed using displacement over 0.5 seconds
+    #Change halfSecFrames as necessary if you need a smaller window to average
+    halfSecFrames=fps/2
+    
+    for i in range(5,tailCords.shape[0]-1):
+        mouseVelocity[i]=distance(tailDataFiltered[i,0],tailDataFiltered[i-halfSecFrames,0],tailDataFiltered[i,1],tailDataFiltered[i-halfSecFrames,1])
+    
+    # Calculate turning as deltas of angles between body and tail, body and nose
+    
+    #               / \
+    #              / C \
+    #             /     \     
+    #            /       \
+    #         b /         \ a
+    #          /           \
+    #         /             \
+    #        /A ___________ B\
+    #                c
+    
+    # Note on how this is calculated: The lengths between nose-body and tail-body 
+    # form an isosceles triangle. The side lengths of this triangle are able to be 
+    # calculated using the distance formula. To calculate an angle in an isosceles triangle
+    # with known side lengths, the cosine rule is used.
+    # Cosine Rule: a^2 = b^2 + c^2 - 2bc * cos(A). 
+    
+    # The angle I'm interested in for the mouse is angle tail->body->nose, which
+    # would be the angle opposite of a line connecting the nose and tail in this
+    # hypothetical triangle. Therefore, in the formula above, length a is the distance
+    # between the nose and the tail. We are solving for the sides, then the angle
+    # for every point in the loop below
+    
+    # MAJOR ADVANTAGE OF THIS METHOD:
+    # Unilateral manipulation of the brain tends to cause biases toward movement in
+    # one direction or the other. Because we are using distances to determine body
+    # angle, the change in body angle is agnostic to the direction of turning. Therefore,
+    # it makes no difference if the animal turns right or left, or if the manipulation is
+    # on the right or left side. We are measuring a direction-agnostic change in body angle
+    # so we don't have to group by the side the manipulation is performed on.
+    a=np.zeros(np.shape(noseCords[:,0]))
+    b=np.zeros(np.shape(bodyCords[:,0]))
+    c=np.zeros(np.shape(tailCords[:,0]))
+    angle=np.zeros(np.shape(noseCords[:,0]))
+    for i in range(tailCords.shape[0]-1):
+        a[i]=distance(noseDataFiltered[i,0],tailDataFiltered[i,0],noseDataFiltered[i,1],tailDataFiltered[i,1])
+        b[i]=distance(tailDataFiltered[i,0],bodyDataFiltered[i,0],tailDataFiltered[i,1],bodyDataFiltered[i,1])
+        c[i]=distance(bodyDataFiltered[i,0],noseDataFiltered[i,0],bodyDataFiltered[i,1],noseDataFiltered[i,1])
+        # We are solving for angle A
+        angle[i]=np.arccos( (-a[i]**2 + b[i]**2 + c[i]**2) / (2*b[i]*c[i]) )
+        
+        #angle[i] is a coordinate in radians. To convert radians back to degrees,
+        #we divide 180 by Pi and multiply the result value by radians
+        angle[i]=180/np.pi*angle[i]
+        
+    #%%  Cell for correlating with laser
+    
+    
+    laserStruct=sio.loadmat(laser_path)
+    
+    #Create new arrays with laser times converted to frame number
+    laserOnTimes=np.zeros(num_events)
+    laserOffTimes=np.zeros(num_events)
+    for i in range(num_events):
+        laserOnTimes[i]=round(laserStruct['LaserOnTimes'][i][0]*fps)
+        laserOffTimes[i]=round(laserStruct['LaserOffTimes'][i][0]*fps)
+    
+    #Extract 0.5 second windows around laser On & Off to see if there's an effect
+    #In these arrays, column 0 is pre-laser and column 1 is post-laser
+    alignedVelocityLaserOn=np.zeros((num_events,2),)
+    alignedVelocityLaserOff=np.zeros((num_events,2),)
+    #Repeat for body angle
+    alignedTurningLaserOn=np.zeros((num_events,2),)
+    alignedTurningLaserOff=np.zeros((num_events,2),)
+    for i in range(num_events):
+        alignedVelocityLaserOn[i,0]=mouseVelocity[int(laserOnTimes[i]-fps/2)]
+        alignedVelocityLaserOn[i,1]=mouseVelocity[int(laserOnTimes[i]+fps/2)]
+        alignedVelocityLaserOff[i,0]=mouseVelocity[int(laserOffTimes[i]-fps/2)]
+        alignedVelocityLaserOff[i,1]=mouseVelocity[int(laserOffTimes[i]+fps/2)]
+        
+        alignedTurningLaserOn[i,0]=angle[int(laserOnTimes[i]-fps/2)]
+        alignedTurningLaserOn[i,1]=angle[int(laserOnTimes[i]+fps/2)]
+        alignedTurningLaserOff[i,0]=angle[int(laserOffTimes[i]-fps/2)]
+        alignedTurningLaserOff[i,1]=angle[int(laserOffTimes[i]+fps/2)]
+    #%% Convert all lists to means for final averaging
+    alignedVelocityLaserOn=[np.mean(alignedVelocityLaserOn[:,0]),np.mean(alignedVelocityLaserOn[:,1])]
+    alignedVelocityLaserOff=[np.mean(alignedVelocityLaserOff[:,0]),np.mean(alignedVelocityLaserOff[:,1])]
+    alignedTurningLaserOn=[np.mean(alignedTurningLaserOn[:,0]),np.mean(alignedTurningLaserOn[:,1])]
+    alignedTurningLaserOff=[np.mean(alignedTurningLaserOff[:,0]),np.mean(alignedTurningLaserOff[:,1])]
+    #%%
+    return([animalName,alignedVelocityLaserOn,alignedVelocityLaserOff,alignedTurningLaserOn,alignedTurningLaserOff]) 
+
+
+    
---
+++ laserAnalyze.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Script for using deepLabCut_FiltLaser to analyze groups of animals
+and average across group.
+
+We are analyzing for effects as a laser turns on (on_pre -> on_post) and for
+effects as a laser turns off (off_pre -> off_post)
+
+Currently no statistics are being taken to determine whether effect sizes are
+signficantly significant. Sample data taken from open field sessions that are
+not comparable (bilateral vs unilateral). For unilateral laser manipulations, 
+this code should suffice because the method of measuring body angle is agnostic
+to which direction the animal turns.
+
+Make sure to define variables (fps, num events) below for your specific project
+
+@author: Luigim Vargas
+June 8th, 2023
+"""
+from tkinter import filedialog
+import tkinter as tk
+import pandas as pd
+import numpy as np
+from deepLabCut_FiltLaser import deepLabCut_FiltLaser
+
+#DEFINE VARIABLES
+#Change these lines as needed
+nAnimals=2 
+fps=10
+num_events=80
+
+
+
+dataStorage=pd.DataFrame()
+
+vel_on_pre=list()
+vel_on_post=list()
+vel_off_pre=list()
+vel_off_post=list()
+
+angle_on_pre=list()
+angle_on_post=list()
+angle_off_pre=list()
+angle_off_post=list()
+for i in range(nAnimals):
+    #create a window so GUI opens above IDE
+    window = tk.Tk()
+    window.wm_attributes('-topmost', 1)
+    window.withdraw()   #close unnecessary tk window
+    coordinate_path = filedialog.askopenfilename(parent = window)
+    laser_path = filedialog.askopenfilename(parent = window)
+    #Change line below to fit your video's fps & number of laser events
+    dataStorage[i]=deepLabCut_FiltLaser(coordinate_path,laser_path,fps,num_events)
+    vel_on_pre.append(dataStorage[i][1][0])
+    vel_on_post.append(dataStorage[i][1][1])
+    vel_off_pre.append(dataStorage[i][2][0])
+    vel_off_post.append(dataStorage[i][2][1])
+
+    angle_on_pre.append(dataStorage[i][3][0])
+    angle_on_post.append(dataStorage[i][3][1])
+    angle_off_pre.append(dataStorage[i][4][0])
+    angle_off_post.append(dataStorage[i][4][1])
+
+
+#%% Graph Results
+
+from matplotlib import pyplot as plt 
+
+#Function for repeated plotting. turningOn will keep track of whether the data corresponds
+#to the data turning On or Off to color points appropriately.
+def laserPlot(pre,post,turningOn):
+    if turningOn==True:
+        colorPre=[0.9,0.9,0.9]
+        colorPost='g'
+    else:
+        colorPre='g'
+        colorPost=[0.9,0.9,0.9]
+    for i in range(nAnimals):
+        plt.scatter(1,pre[i],color=colorPre)
+        plt.scatter(2,post[i],color=colorPost)
+        plt.plot([1,2],[pre[i],post[i]],color=[0.9,0.9,0.9])
+    plt.plot([1,2],[np.mean(pre[:]),np.mean(post[:])],color='k')    
+    return
+
+plt.subplot(2,2,1)
+laserPlot(vel_on_pre,vel_on_post,True)
+plt.xticks([1,2],labels=["Laser Off","Laser On"])
+plt.xlim([0,3])
+plt.ylabel("Velocity (AU)")
+plt.title([" Laser Turning On Effect on Velocity"])
+
+plt.subplot(2,2,2)
+laserPlot(vel_off_pre,vel_off_post,False)
+plt.xticks([1,2],labels=["Laser On","Laser Off"])
+plt.xlim([0,3])
+plt.ylabel("Velocity (AU)")
+plt.title([" Laser Turning Off Effect on Velocity"])
+
+plt.subplot(2,2,3)
+laserPlot(angle_on_pre,angle_on_post,True)
+plt.xticks([1,2],labels=["Laser Off","Laser On"])
+plt.xlim([0,3])
+plt.ylabel("Body Angle (Degrees)")
+plt.title([" Laser Turning On Effect on Body Angle"])
+
+plt.subplot(2,2,4)
+laserPlot(angle_off_pre,angle_off_post,False)
+plt.xticks([1,2],labels=["Laser On","Laser Off"])
+plt.xlim([0,3])
+plt.ylabel("Body Angle (Degrees)")
+plt.title([" Laser Turning Off Effect on Body Angle"])
